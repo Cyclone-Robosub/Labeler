1c3d6eb26c956e2ae09b81a91d32d0f63a2885e0
AlundorZhu <ziqzhu@ucdavis.edu>
2025-08-15 16:13:52 -0700

    training script used

--- yolo/train.py
+++ yolo/train.py
@@ -1,19 +1,16 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("/home/kid/roboSub/Labeler/july_27_best.pt")  # load a pretrained model (recommended for training)
+model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
 
 # Train using the single most idle GPU
 results = model.train(
-    data="/home/kid/roboSub/Labeler/data.yaml", 
+    data="/home/kid/roboSub/Labeler/yolo/data.yaml", 
     epochs=300,
     patience=30,
 
-    imgsz=512,
-    batch=128,
-
     device="cuda",
-    project="Block detector",
+    project="RoboSub_gate",
     # save_period=10,
 
     augment=True,
